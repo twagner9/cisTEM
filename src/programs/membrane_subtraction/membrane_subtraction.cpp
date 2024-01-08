dc7e4ab11e923b93c702461f82bc7acc17b0a1a4
Tim <timwagner240@gmail.com>
2024-01-08 09:02:11 -0600

    Membrane subtraction compiles
    No longer bothering with reading into memory at the moment. Instead, we just use the typical ReadSlice functions as needed as the entire stack of images only needs to be accessed once.

--- src/programs/membrane_subtraction/membrane_subtraction.cpp
+++ src/programs/membrane_subtraction/membrane_subtraction.cpp
@@ -1,6 +1,6 @@
 #include "../../core/core_headers.h"
 
-class MembraneSubtraction : MyApp {
+class MembraneSubtraction : public MyApp {
   public:
     void DoInteractiveUserInput( );
     bool DoCalculation( );
@@ -37,17 +37,21 @@ typedef struct ctf_parameters {
  * declared just as <return_type> <function_name>.
  * This also means they can be called without being attached to an object
 */
-void generate_reference_image(Image* input_stack, Image* input_stack_times_ctf, MRCFile* input_file, ctf_parameters* ctf_params, int number_of_images, int); // Like azimuthal average, only using the 2D class .star -- also probably not needed
 void apply_ctf( ); // Probably unneeded
-void apply_shifts_and_rotations( );
-void scale_and_subtract( );
+void read_images_and_ctf(Image* class_average, MRCFile* all_particles_mrc, Image* class_members_stack, ctf_parameters* class_average_ctf_params, cisTEMParameters classification_parameters, NumericTextFile* class_average_members, long number_of_images, int selected_class_average, int number_of_threads, bool use_memory);
+void rotate_shift_scale_subtract(MRCFile* particle_mrc, MRCFile* classification_mrc, cisTEMParameters ctf_params, wxArrayLong* class_members, int class_average_index, long number_of_images, long number_of_members);
+
+IMPLEMENT_APP(MembraneSubtraction)
 
 // Get all user inputs necessary for running the program
 void MembraneSubtraction::DoInteractiveUserInput( ) {
-    UserInput*  my_input                            = new UserInput( );
-    std::string extracted_particle_members_filename = my_input->GetFilenameFromUser("Input text file containing particle positions", "The name of the text file containing all members of the class average", "extracted_particles.txt", true);
-    std::string input_mrc_filename                  = my_input->GetFilenameFromUser("Input particle stack", "The filename for the relevant .mrc file", "input.mrc", true);
-    std::string input_star_file                     = my_input->GetFilenameFromUser("Input parameters file", "Enter the name of the input parameters file associated with the refinement package containing the extracted particle members of the class average", "input_star_parameters.star", true);
+    UserInput*  my_input                    = new UserInput("Subtraction of 2D Class Averages", 1.00);
+    std::string database_filename           = my_input->GetFilenameFromUser("Database filename that contains relevant classification", "cisTEM .db file containing the class average that should be subtracted", "input_database.db", true);
+    int         classification_id           = my_input->GetIntFromUser("Input the classification ID that contains the class average for subtraction", "Class ID of class average being used for subtraction", "1");
+    int         class_average_index         = my_input->GetIntFromUser("Enter the position of the class average in the classification mrc that is needed for subtraction", "The index of the class average within its classification", "1");
+    std::string particle_stack_filename     = my_input->GetFilenameFromUser("Input particle stack", "The filename for the relevant .mrc file", "input.mrc", true);
+    std::string input_star_file             = my_input->GetFilenameFromUser("Input parameters file", "Enter the name of the input parameters file associated with the refinement package containing the extracted particle members of the class average", "input_star_parameters.star", true);
+    std::string classification_mrc_filename = my_input->GetFilenameFromUser("Input the relevant classification containing the class average for subtraction", "Classifications have multiple 2D class averages within them; provide the file of the relevant classification.", "class_averages.mrc", true);
 
     /* This is an additional option that lets user's decide to put the images directly into memory or not; generally, you want to be able to do this, as it will
      * reduce the program's runtime, but if you have a very large stack but limited memory, it's not always feasible. So by adding this option,
@@ -72,38 +76,35 @@ void MembraneSubtraction::DoInteractiveUserInput( ) {
 // This ends the directive, so #ifdef and #else (and other precompiler directives) are no longer sought by the compiler
 #endif
 
-    my_current_job.Reset(5);
-    my_current_job.ManualSetArguments("tttbi", extracted_particle_members_filename.c_str( ),
-                                      input_mrc_filename.c_str( ),
+    my_current_job.Reset(8);
+    my_current_job.ManualSetArguments("tiitttbi", database_filename.c_str( ),
+                                      classification_id,
+                                      class_average_index,
+                                      particle_stack_filename.c_str( ),
                                       input_star_file.c_str( ),
+                                      classification_mrc_filename.c_str( ),
                                       use_memory,
                                       max_threads);
 }
 
 // The main function that gets executed when the program is run
 // This will also contain calls to the forward declared functions above
-bool DoCalculation( ) {
-    std::string extracted_particle_members_filename = my_current_job.arguments[0].ReturnStringArgument( );
-    std::string input_stack_filename                = my_current_job.arguments[1].ReturnStringArgument( );
-    wxString    star_filename                       = wxString(my_current_job.arguments[2].ReturnStringArgument( ));
-    bool        use_memory                          = my_current_job.arguments[3].ReturnBoolArgument( );
-    int         number_of_threads                   = my_current_job.arguments[4].ReturnIntegerArgument( );
+bool MembraneSubtraction::DoCalculation( ) {
+    wxFileName  database_filename            = wxFileName(my_current_job.arguments[0].ReturnStringArgument( ));
+    const int   classification_id            = my_current_job.arguments[1].ReturnIntegerArgument( );
+    const int   class_average_index          = my_current_job.arguments[2].ReturnIntegerArgument( );
+    std::string particle_stack_filename      = my_current_job.arguments[3].ReturnStringArgument( );
+    std::string input_star_filename          = my_current_job.arguments[4].ReturnStringArgument( );
+    std::string classifications_mrc_filename = my_current_job.arguments[5].ReturnStringArgument( );
+    bool        use_memory                   = my_current_job.arguments[6].ReturnBoolArgument( );
+    int         number_of_threads            = my_current_job.arguments[7].ReturnIntegerArgument( );
 
     // Get the stack into usable form
-    MRCFile input_stack(input_stack_filename, false);
-    long    number_of_images = input_stack.ReturnNumberOfSlices( );
 
-    // Get the star params into usable form
-    cisTEMParameters input_star_parameters;
-    input_star_parameters.ReadFromcisTEMStarFile(star_filename, true);
-
-    // Read in the extracted members
-    NumericTextFile particle_positions_list = new NumericTextFile(extracted_particle_members_filename, OPEN_TO_READ);
     // TODO: add checks for list consitency with member count (the .mrc of class members length, I believe)
 
     // Check to see if the nubmer of images in the stack matches
     // the number of lines in the star file
-
     // NOTE: this check does not work; the .star will probably have all members of the original stack, but we're
     // only subtracting from the members of the class, so .star SHOULD have more lines
     /*if ( number_of_images != input_star_parameters.ReturnNumberofLines( ) ) {
@@ -111,15 +112,12 @@ bool DoCalculation( ) {
         DEBUG_ABORT;
     }*/
 
-    // Initialize CTF parameters to an array equal in size to the number of images we actually need
-    ctf_parameters* ctf_params = new ctf_parameters[particle_positions_list.ReturnNumberofLines( )];
-
     // These will allow us to track the amount of time the program takes to run, as well as individual steps;
-    // especially useful for comparing between memory-allocation and no mem allocation runtimes
+    // especially useful for comparing between memory-allocation and no mem allocation runtimes.
     // Also serves to make sure the user can tell visually that the program is running properly
     wxDateTime overall_start = wxDateTime::Now( );
     wxDateTime overall_finish;
-    wxDateTime read_frames_start;
+    /*wxDateTime read_frames_start;
     wxDateTime read_frames_finish;
     wxDateTime alignment_start;
     wxDateTime alignment_finish;
@@ -128,40 +126,31 @@ bool DoCalculation( ) {
 
     wxPrintf("\nReading Images...\n");
     read_frames_start = wxDateTime::Now( );
+*/
 
-    // Now we deal with the user's choice to use memory or not
-    Image* image_stack;
-    Image* input_stack_times_ctf;
-    if ( use_memory ) {
-        image_stack           = new Image[number_of_images];
-        input_stack_times_ctf = new Image[number_of_images];
-        for ( int image_counter = 1; image_counter < number_of_images; image_counter++ ) {
-            image_stack.ReadSlice(&input_stack, image_counter);
-        }
-    }
-    else {
-        image_stack           = nullptr;
-        input_stack_times_ctf = nullptr;
-    }
+    // Open db and prepare the refinement packages
+    Database selected_db = Database( );
+    selected_db.Open(database_filename);
 
-    /* TODO: Continue with setup (getting arrays storing x shifts, y shifts, psi angles, defocus values...or just pull from the params as needed?)
-     * Becomes a question of how often we will need to use each of the parameters/what the overhead -- i.e., time/processing cost -- would be for having such function calls
-     * versus just allocating to memory
-     * To be honest, I think we only need to access these 2 or 3 times maximally; once for setting up the CTF, once for setting up the 2D...and something else I can't think of atm
-     * I'm not positive on this however.
-    */
-    // For post read:
-    read_frames_finish      = wxDateTime::Now( );
-    wxTimeSpan time_to_read = read_frames_finish.Subtract(read_frames_start);
-    wxPrintf("Read frames runtime: %s\n", time_to_read.Format( ));
+    // This is the positions in the particle stack of all the class average members
+    wxArrayLong class_members     = selected_db.Return2DClassMembers(classification_id, class_average_index);
+    long        number_of_members = class_members.GetCount( );
+    selected_db.Close(false);
+
+    // Read in necessary .mrc(s) files
+    MRCFile particle_stack_mrc(particle_stack_filename, false);
+    MRCFile classification_mrc(classifications_mrc_filename, false);
+    long    number_of_images = particle_stack_mrc.ReturnNumberOfSlices( );
 
     // Read in CTF params
-    // Constants first, pulled from first line of star file
-    const float acceleration_voltage = input_star_parameters.ReturnMicroscopeKv(1);
-    const float spherical_aberration = input_star_parameters.ReturnMicroscopeCs(1);
-    const float pixel_size           = input_star_parameters.ReturnPixelSize(1);
+    cisTEMParameters input_star_parameters;
+    input_star_parameters.ReadFromcisTEMStarFile(input_star_filename, true);
 
-    float temp_float[1]; // Just temporarily stores each line from our .txt of extracted members
+    // Initialize CTF parameters to an array equal in size to the number of images we actually need
+    /*ctf_parameters* ctf_params = new ctf_parameters[number_of_images];
+
+    float temp_float[3]; // Just temporarily stores each line from our .txt of extracted members
+    long  current_line;
 
     // Loop over .star for reading in the rest
     for ( int particle_counter = 0; particle_counter < number_of_images; particle_counter++ ) {
@@ -170,15 +159,16 @@ bool DoCalculation( ) {
 
         ctf_params[particle_counter].pixel_size = pixel_size;
 
-        particle_positions_list.ReadLine(&temp_float);
+        particle_positions_list->ReadLine(temp_float);
+        current_line = long(temp_float[0]);
 
-        ctf_params[particle_counter].psi                = input_star_parameters.ReturnPsi(temp_float[0]);
-        ctf_params[particle_counter].x_shift            = input_star_parameters.ReturnXShift(temp_float[0]);
-        ctf_params[particle_counter].y_shift            = input_star_parameters.ReturnYShift(temp_float[0]);
-        ctf_parame[particle_counter].defocus_1          = input_star_parameters.ReturnDefocus1(temp_float[0]);
-        ctf_params[particle_counter].defocus_2          = input_star_parameters.ReturnDefocus2(temp_float[0]);
-        ctf_params[particle_counter].astigmatism_angle  = input_star_parameters.ReturnDefocusAngle(temp_float[0]);
-        ctf_params[particle_counter].amplitude_contrast = input_star_parameters.ReturnAmplitudeContrast(temp_float[0]);
+        ctf_params[particle_counter].psi                = input_star_parameters.ReturnPsi(current_line);
+        ctf_params[particle_counter].x_shift            = input_star_parameters.ReturnXShift(current_line);
+        ctf_params[particle_counter].y_shift            = input_star_parameters.ReturnYShift(current_line);
+        ctf_params[particle_counter].defocus_1          = input_star_parameters.ReturnDefocus1(current_line);
+        ctf_params[particle_counter].defocus_2          = input_star_parameters.ReturnDefocus2(current_line);
+        ctf_params[particle_counter].astigmatism_angle  = input_star_parameters.ReturnDefocusAngle(current_line);
+        ctf_params[particle_counter].amplitude_contrast = input_star_parameters.ReturnAmplitudeContrast(current_line);
 
         // Not needed?
         ctf_params[particle_counter].lowest_frequency_for_fitting  = 0.0f;
@@ -186,12 +176,83 @@ bool DoCalculation( ) {
         ctf_params[particle_counter].astigmatism_tolerance         = 0.0f;
         ctf_params[particle_counter].additional_phase_shift        = 0.0f;
     }
+*/
+    // For post read:
+    //read_frames_finish      = wxDateTime::Now( );
+    //wxTimeSpan time_to_read = read_frames_finish.Subtract(read_frames_start);
+    //wxPrintf("Read frames runtime: %s\n", time_to_read.Format( ));
 
-    CTF current_ctf;
-    if ( use_memory ) {
-        for ( int member_counter = 0; member_counter < number_of_images; member_counter++ ) {
-            current_ctf.Init(ctf_parameters_stack[member_counter].acceleration_voltage, ctf_parameters_stack[member_counter].spherical_aberration, ctf_parameters_stack[member_counter].amplitude_contrast, ctf_parameters_stack[member_counter].defocus_1, ctf_parameters_stack[member_counter].defocus_2, ctf_parameters_stack[member_counter].astigmatism_angle, ctf_parameters_stack[member_counter].lowest_frequency_for_fitting, ctf_parameters_stack[member_counter].highest_frequency_for_fitting, ctf_parameters_stack[member_counter].astigmatism_tolerance, ctf_parameters_stack[member_counter].pixel_size, ctf_parameters_stack[member_counter].additional_phase_shift);
+    // TODO: Add function call for performing rotations and shifts
+    return true;
+}
+
+/**
+ * @brief The function that applies CTF parameters to the class average to subtract the average from the
+ * full stack.
+ * 
+ * @param particle_mrc Particle stack associated with the refinement package that containst the needed classification
+ * @param classification_mrc Classification mrc that has all the class averages from the selected classification
+ * @param ctf_params CTF parameters read in from the user-passed star file
+ * @param class_members Array containing the list of all the members
+ */
+void rotate_shift_scale_subtract(MRCFile* particle_mrc, MRCFile* classification_mrc, cisTEMParameters ctf_params, wxArrayLong* class_members, int class_average_index, long number_of_images, long number_of_members) {
+    Image current_image;
+    Image class_average_image;
+    CTF   current_ctf;
+    long  member_counter = 0;
+    long  current_member;
+
+    // For CTF
+    const float microscope_voltage     = ctf_params.ReturnMicroscopekV(1);
+    const float spherical_abberation   = ctf_params.ReturnMicroscopeCs(1);
+    const float pixel_size             = ctf_params.ReturnPixelSize(1);
+    const float additional_phase_shift = 0.0f;
+    float       psi;
+    float       x_shift;
+    float       y_shift;
+    float       defocus_1;
+    float       defocus_2;
+    float       astigmatism_angle;
+    float       amplitude_contrast;
+
+    // For scaling
+    double sum_of_pixelwise_product;
+    double sum_of_squares;
+    double scale_factor;
+
+    class_average_image.ReadSlice(classification_mrc, class_average_index);
+
+    // FIXME?: Better to just re-read the plain image, or to remove the most recently used CTF?
+    // Unsure of which is more efficient, though removal seems to be more likely to be better
+    for ( int image_counter = 0; image_counter < number_of_images; image_counter ) {
+        current_image.ReadSlice(particle_mrc, image_counter + 1);
+        if ( class_members->Item(member_counter) - 1 == image_counter ) {
+            current_member = class_members->Item(member_counter);
+            // Apply CTF
+            psi                = ctf_params.ReturnPsi(current_member);
+            x_shift            = ctf_params.ReturnXShift(current_member);
+            y_shift            = ctf_params.ReturnYShift(current_member);
+            defocus_1          = ctf_params.ReturnDefocus1(current_member);
+            defocus_2          = ctf_params.ReturnDefocus2(current_member);
+            astigmatism_angle  = ctf_params.ReturnDefocusAngle(current_member);
+            amplitude_contrast = ctf_params.ReturnAmplitudeContrast(current_member);
+
+            current_ctf.Init(microscope_voltage, spherical_abberation, amplitude_contrast, defocus_1, defocus_2, astigmatism_angle, pixel_size, additional_phase_shift);
+            class_average_image.ForwardFFT( );
+            class_average_image.ApplyCTF(current_ctf);
+            class_average_image.BackwardFFT( );
+
+            // All shifts and rotations applied, now scale: A * B / A * A
+            // A is class average, B is current image
+            for ( long pixel_counter = 0; pixel_counter < class_average_image.number_of_real_space_pixels; pixel_counter++ ) {
+                sum_of_pixelwise_product += class_average_image.real_values[pixel_counter] * current_image.real_values[pixel_counter];
+                sum_of_squares += class_average_image.real_values[pixel_counter] * class_average_image.real_values[pixel_counter];
+            }
+            scale_factor = sum_of_pixelwise_product / sum_of_squares;
+
+            class_average_image.MultiplyByConstant(scale_factor);
+            current_image.SubtractImage(&class_average_image);
         }
+        current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
     }
-    // TODO: Add function calls for performing rotations and shifts
 }
