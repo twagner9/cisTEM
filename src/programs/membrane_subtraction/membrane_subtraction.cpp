5256affa36c1a7fd28412895c8fa00752e5e4281
Tim <timwagner240@gmail.com>
2024-01-17 09:21:22 -0600

    Alignment seems better, can subtract multiple averages
    Needed a -1 when accessing an average's members as the cisTEMParameters class has an array with all .star parameters contained within rather than reading from the .star itself.
    Broke up if statement to be nested instead of using &&.

--- src/programs/membrane_subtraction/membrane_subtraction.cpp
+++ src/programs/membrane_subtraction/membrane_subtraction.cpp
@@ -18,7 +18,7 @@ typedef struct ClassAveragesAndMembers {
     wxArrayLong class_members;
 } ClassAveragesAndMembers;
 
-void align_scale_subtract(MRCFile* particle_mrc, MRCFile* classification_mrc, cisTEMParameters* ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images, int max_threads);
+void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images);
 
 IMPLEMENT_APP(MembraneSubtraction)
 
@@ -32,7 +32,7 @@ void MembraneSubtraction::DoInteractiveUserInput( ) {
     std::string classification_mrc_filename    = my_input->GetFilenameFromUser("Input the relevant classification containing the class average for subtraction", "Classifications have multiple 2D class averages within them; provide the file of the relevant classification.", "class_averages.mrc", true);
 
     int max_threads;
-
+    /*
 #ifdef _OPENMP
     max_threads = my_input->GetIntFromUser("Max number of threads", "The maximum number of threads to be used during calculations", "1");
 #else
@@ -40,7 +40,7 @@ void MembraneSubtraction::DoInteractiveUserInput( ) {
     max_threads = 1;
 // This ends the directive, so #ifdef and #else (and other precompiler directives) are no longer sought by the compiler
 #endif
-
+*/
     delete my_input;
 
     my_current_job.Reset(6);
@@ -84,7 +84,6 @@ bool MembraneSubtraction::DoCalculation( ) {
 
     for ( int average_counter = 0; average_counter < number_of_averages; average_counter++ ) {
         class_average_indices_list->ReadLine(tmp_float);
-        wxPrintf("\nRead average = %f\n", tmp_float[0]);
         list_of_averages_and_members[average_counter].class_average_index = long(tmp_float[0]);
         list_of_averages_and_members[average_counter].class_members       = selected_db.Return2DClassMembers(classification_id, tmp_float[0]);
     }
@@ -114,18 +113,6 @@ bool MembraneSubtraction::DoCalculation( ) {
     needed_class->WritecisTEMStarFile("extracted_subtraction_class", &needed_package, false);
     std::string star_filename = "extracted_subtraction_class_" + std::to_string(classification_id) + ".star"; // so-so method if .star naming convention ever changes
 
-    // Write member positions to a text file so they can be found within the stack when reviewing subtraction
-    // This is probably no longer worth doing -- in any case, I know that subtraction has worked up to this point
-    /*
-    std::ofstream my_file;
-    std::string   text_filename = "extracted_particles_" + std::to_string(classification_id) + "_" + std::to_string(class_average_index) + ".txt";
-    my_file.open(text_filename);
-    for ( int particle_counter = 0; particle_counter < class_members.GetCount( ); particle_counter++ )
-        my_file << class_members[particle_counter] << std::endl;
-
-    my_file.close( );
-    */
-
 #ifdef TMP_DEBUG
     wxPrintf("\nRead in all class averages and their members.\n");
     wxPrintf("About to read in particle stack and classification MRCs\n");
@@ -153,7 +140,7 @@ bool MembraneSubtraction::DoCalculation( ) {
     wxPrintf("STAR file read; now about to enter rotate_shift_scale_subtract.\n");
 #endif
 
-    align_scale_subtract(&particle_stack_mrc, &classification_mrc, &input_star_parameters, list_of_averages_and_members, number_of_averages, number_of_images, number_of_threads);
+    align_scale_subtract(particle_stack_mrc, classification_mrc, input_star_parameters, list_of_averages_and_members, number_of_averages, number_of_images);
 
     overall_finish            = wxDateTime::Now( );
     wxTimeSpan time_to_finish = overall_finish.Subtract(overall_start);
@@ -171,26 +158,22 @@ bool MembraneSubtraction::DoCalculation( ) {
  * @param class_members Array containing the list of all the members
  * @param list_of_averages_and_members Vector containing the grouped class averages and their included members
  * @param number_of_images Number of images in the full particle stack
- * @param max_threads Maximum number of threads available to OpenMP for calculations
  */
-void align_scale_subtract(MRCFile* particle_mrc, MRCFile* classification_mrc, cisTEMParameters* ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images, int max_threads) {
-    Image       current_image;
-    Image       class_average_image;
-    CTF         current_ctf;
-    long        member_counter = 0;
-    long        image_counter;
-    long        pixel_counter;
-    long        current_member;
-    long        number_of_members;
-    int         current_class_average_index;
-    wxArrayLong current_members_list;
-    bool        image_is_already_written = false;
+void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images) {
+    Image current_image;
+    Image class_average_image;
+    CTF   current_ctf;
+    long  image_counter;
+    long  pixel_counter;
+    long  current_member;
+    int   current_class_average_index;
+    bool  image_is_already_written = false;
 
     // For CTF
-    const float microscope_voltage     = ctf_params->ReturnMicroscopekV(1);
-    const float spherical_abberation   = ctf_params->ReturnMicroscopeCs(1);
-    const float pixel_size             = ctf_params->ReturnPixelSize(1);
-    const float additional_phase_shift = ctf_params->ReturnPhaseShift(1);
+    const float microscope_voltage     = ctf_params.ReturnMicroscopekV(0);
+    const float spherical_abberation   = ctf_params.ReturnMicroscopeCs(0);
+    const float pixel_size             = ctf_params.ReturnPixelSize(0);
+    const float additional_phase_shift = ctf_params.ReturnPhaseShift(0); // Only non-const if phase plate is used?
     float       psi;
     float       x_shift;
     float       y_shift;
@@ -204,78 +187,60 @@ void align_scale_subtract(MRCFile* particle_mrc, MRCFile* classification_mrc, ci
     double sum_of_squares;
     double scale_factor;
 
-    //#pragma omp parallel num_threads(max_threads) default(none) shared(microscope_voltage, spherical_abberation, pixel_size, additional_phase_shift, number_of_images, ctf_params, classification_mrc, particle_mrc, list_of_averages_and_members, member_counter, number_of_averages) private(current_image, class_average_image, current_ctf, image_counter, current_member, pixel_counter, psi, x_shift, y_shift, defocus_1, defocus_2, astigmatism_angle, amplitude_contrast, sum_of_pixelwise_product, sum_of_squares, scale_factor, number_of_members, current_class_average_index, current_members_list, image_is_already_written)
-    { // begin omp
-        //#pragma omp for ordered
-        for ( image_counter = 0; image_counter < number_of_images; image_counter++ ) {
-            current_image.ReadSlice(particle_mrc, image_counter + 1);
-            image_is_already_written = false;
-            wxPrintf("\nIn the image counting loop");
+    for ( image_counter = 0; image_counter < number_of_images; image_counter++ ) {
+        current_image.ReadSlice(&particle_mrc, image_counter + 1);
+        image_is_already_written = false;
 
-            // Get the average and its members ready to subtract
-            for ( int class_average_counter = 0; class_average_counter < number_of_averages; class_average_counter++ ) {
-                current_class_average_index = list_of_averages_and_members[class_average_counter].class_average_index;
-                current_members_list        = list_of_averages_and_members[class_average_counter].class_members;
-                number_of_members           = current_members_list.GetCount( );
-                wxPrintf("\nIn the class averages loop");
+        // Get the average and its member ready to subtract
+        for ( int class_average_counter = 0; class_average_counter < number_of_averages; class_average_counter++ ) {
+            if ( list_of_averages_and_members[class_average_counter].member_counter < list_of_averages_and_members[class_average_counter].number_of_members ) {
+                if ( list_of_averages_and_members[class_average_counter].class_members.Item(list_of_averages_and_members[class_average_counter].member_counter) - 1 == image_counter ) {
+                    class_average_image.ReadSlice(&classification_mrc, list_of_averages_and_members[class_average_counter].class_average_index);
 
-                while ( member_counter < number_of_members && ! image_is_already_written ) {
-                    wxPrintf("\nEntered WHILE LOOP");
-                    //#pragma omp ordered
-                    if ( member_counter < number_of_members && current_members_list.Item(member_counter) - 1 == image_counter ) {
-                        class_average_image.ReadSlice(classification_mrc, current_class_average_index);
-                        current_member = current_members_list.Item(member_counter);
-                        //#pragma omp critical
-                        ++member_counter;
-#ifdef TMP_DEBUG
-                        wxPrintf("\nmember_counter = %li", member_counter);
-#endif
+                    // -1 because the cisTEMParameters class contains an array of all params -- this means 0 indexing
+                    current_member = list_of_averages_and_members[class_average_counter].class_members.Item(list_of_averages_and_members[class_average_counter].member_counter) - 1;
 
-                        // Read in CTF
-                        psi                = ctf_params->ReturnPsi(current_member);
-                        x_shift            = ctf_params->ReturnXShift(current_member);
-                        y_shift            = ctf_params->ReturnYShift(current_member);
-                        defocus_1          = ctf_params->ReturnDefocus1(current_member);
-                        defocus_2          = ctf_params->ReturnDefocus2(current_member);
-                        astigmatism_angle  = ctf_params->ReturnDefocusAngle(current_member);
-                        amplitude_contrast = ctf_params->ReturnAmplitudeContrast(current_member);
+                    // Read in CTF params
+                    psi                = ctf_params.ReturnPsi(current_member);
+                    x_shift            = ctf_params.ReturnXShift(current_member);
+                    y_shift            = ctf_params.ReturnYShift(current_member);
+                    defocus_1          = ctf_params.ReturnDefocus1(current_member);
+                    defocus_2          = ctf_params.ReturnDefocus2(current_member);
+                    astigmatism_angle  = ctf_params.ReturnDefocusAngle(current_member);
+                    amplitude_contrast = ctf_params.ReturnAmplitudeContrast(current_member);
 
-                        //Apply to class average
-                        current_ctf.Init(microscope_voltage, spherical_abberation, amplitude_contrast, defocus_1, defocus_2, astigmatism_angle, pixel_size, additional_phase_shift);
-                        class_average_image.ForwardFFT( );
-                        class_average_image.ZeroCentralPixel( );
-                        class_average_image.ApplyCTF(current_ctf);
-                        class_average_image.BackwardFFT( );
+                    //Apply to class average
+                    current_ctf.Init(microscope_voltage, spherical_abberation, amplitude_contrast, defocus_1, defocus_2, astigmatism_angle, pixel_size, additional_phase_shift);
+                    class_average_image.ForwardFFT( );
+                    class_average_image.ZeroCentralPixel( );
+                    class_average_image.ApplyCTF(current_ctf);
+                    class_average_image.BackwardFFT( );
 
-                        // Align
-                        class_average_image.PhaseShift(-x_shift, -y_shift);
-                        class_average_image.Rotate2DInPlace(-psi);
-                        //class_average_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", current_member);
+                    // Align
+                    class_average_image.Rotate2DInPlace(-psi);
+                    class_average_image.PhaseShift(-x_shift, -y_shift);
+                    class_average_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", image_counter + 1);
 
-                        // Scale: A * B / A * A
-                        // A is class average, B is current image
-                        wxPrintf("\nCalculating scale factor...");
-                        for ( pixel_counter = 0; pixel_counter < class_average_image.number_of_real_space_pixels; pixel_counter++ ) {
-                            sum_of_pixelwise_product += class_average_image.real_values[pixel_counter] * current_image.real_values[pixel_counter];
-                            sum_of_squares += class_average_image.real_values[pixel_counter] * class_average_image.real_values[pixel_counter];
-                        }
-                        scale_factor = sum_of_pixelwise_product / sum_of_squares;
-
-                        class_average_image.MultiplyByConstant(scale_factor);
-                        current_image.SubtractImage(&class_average_image);
-                        current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
-                        image_is_already_written = true;
+                    // Scale: A * B / A * A
+                    // A is class average, B is current image
+                    for ( pixel_counter = 0; pixel_counter < class_average_image.number_of_real_space_pixels; pixel_counter++ ) {
+                        sum_of_pixelwise_product += class_average_image.real_values[pixel_counter] * current_image.real_values[pixel_counter];
+                        sum_of_squares += class_average_image.real_values[pixel_counter] * class_average_image.real_values[pixel_counter];
                     }
-                    else {
-                        member_counter++;
-                    }
-                    if ( image_is_already_written )
-                        break;
+                    scale_factor = sum_of_pixelwise_product / sum_of_squares;
+
+                    class_average_image.MultiplyByConstant(scale_factor);
+                    current_image.SubtractImage(&class_average_image);
+                    current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
+                    image_is_already_written = true;
+                    list_of_averages_and_members[class_average_counter].member_counter++;
+                    break; // Did what we needed; now done with class averages and should move to next image before looping them again.
                 }
             }
-            //#pragma omp ordered
-            if ( ! image_is_already_written )
-                current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
         }
-    } // end omp
+        if ( ! image_is_already_written ) {
+            current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
+            current_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", image_counter + 1); // Filler for sanity check -- ask Tim how to improve this
+        }
+    }
 }
