143bae7f50026e20e378bf94b49b12d53a662d90
Tim <timwagner240@gmail.com>
2024-01-22 10:35:45 -0600

    Recreate sums before subtraction
    Filtering and other manipulations may be done when creating class averages; this interferes with effective subtraction from images.
    To fix this, we create a sum of each average using its member particles; this sum lacks any manipulations that will reduce effectiveness of subtraction.
    
    Added functions for applying and CTF and removing CTF (divide_by_ctf_sum_of_squares).

--- src/programs/membrane_subtraction/membrane_subtraction.cpp
+++ src/programs/membrane_subtraction/membrane_subtraction.cpp
@@ -1,9 +1,7 @@
 #include "../../core/core_headers.h"
 #define TMP_DEBUG
 
-//TODO: DO need to write out the star file; will need this for importing back into cisTEM and for reading
-// I suppose this raises the question of whether it's better to keep the .star writing and member extraction
-// separate from the main subtraction program
+//TODO: better to just create a ctf_params struct for holding the params since they are now being used in 2 separate places?
 class MembraneSubtraction : public MyApp {
   public:
     void DoInteractiveUserInput( );
@@ -12,15 +10,18 @@ class MembraneSubtraction : public MyApp {
   private:
 };
 
-// NOTE: made to ensure proper association of the class_average_index and its members
+// Made to ensure proper association of the class_average_index and its members
 typedef struct ClassAveragesAndMembers {
     int         class_average_index;
     long        number_of_members;
     long        member_counter = 0;
     wxArrayLong class_members;
+    bool        remade_average_is_written = false;
 } ClassAveragesAndMembers;
 
-void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images);
+void align_scale_subtract(MRCFile& particle_mrc, Image* raw_averages_to_subtract, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images);
+void apply_ctf(Image& current_image, CTF& ctf_to_apply, float* ctf_sum_of_squares, bool absolute = false);
+void divide_by_ctf_sum_of_squares(Image& current_image, float* ctf_sum_of_squares);
 
 IMPLEMENT_APP(MembraneSubtraction)
 
@@ -119,9 +120,9 @@ bool MembraneSubtraction::DoCalculation( ) {
     wxPrintf("About to read in particle stack and classification MRCs\n");
 #endif
     // Read in necessary .mrc(s) files
-    MRCFile particle_stack_mrc(particle_stack_filename, false);
-    MRCFile classification_mrc(classifications_mrc_filename, false);
-    long    number_of_images = particle_stack_mrc.ReturnNumberOfSlices( );
+    MRCFile    particle_stack_mrc(particle_stack_filename, false);
+    MRCFile    classification_mrc(classifications_mrc_filename, false);
+    const long number_of_images = particle_stack_mrc.ReturnNumberOfSlices( );
 
 #ifdef TMP_DEBUG
     wxPrintf("MRCs read in; number of images = %li; now read in .STAR params\n", number_of_images);
@@ -137,11 +138,78 @@ bool MembraneSubtraction::DoCalculation( ) {
         DEBUG_ABORT;
     }
 
+    // Recreate the class average from the particle stack
+    Image       current_test_average;
+    Image       current_image;
+    const float microscope_voltage     = input_star_parameters.ReturnMicroscopekV(0);
+    const float spherical_abberation   = input_star_parameters.ReturnMicroscopeCs(0);
+    const float pixel_size             = input_star_parameters.ReturnPixelSize(0);
+    const float additional_phase_shift = input_star_parameters.ReturnPhaseShift(0); // Only non-const if phase plate is used?
+    float       psi;
+    float       x_shift;
+    float       y_shift;
+    float       defocus_1;
+    float       defocus_2;
+    float       astigmatism_angle;
+    float       amplitude_contrast;
+    CTF         tmp_ctf;
+
+    current_image.ReadSlice(&particle_stack_mrc, 1);
+    current_test_average.Allocate(current_image.logical_x_dimension, current_image.logical_y_dimension, false);
+
+    float* ctf_sum_of_squares_array = new float[current_image.real_memory_allocated / 2];
+
+    Image* tmp_averages = new Image[number_of_averages];
+
+    // Hate to do it, but must loop over full stack for each average to get proper sums
+    for ( int average_counter = 0; average_counter < number_of_averages; average_counter++ ) {
+        current_test_average.SetToConstant(0.0);
+        ZeroFloatArray(ctf_sum_of_squares_array, current_image.real_memory_allocated / 2);
+        while ( list_of_averages_and_members[average_counter].member_counter < list_of_averages_and_members[average_counter].number_of_members ) {
+            // Get needed image
+            current_image.ReadSlice(&particle_stack_mrc, list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter));
+
+            // Get params
+            psi                = input_star_parameters.ReturnPsi(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            x_shift            = input_star_parameters.ReturnXShift(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            y_shift            = input_star_parameters.ReturnYShift(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            defocus_1          = input_star_parameters.ReturnDefocus1(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            defocus_2          = input_star_parameters.ReturnDefocus2(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            astigmatism_angle  = input_star_parameters.ReturnDefocusAngle(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+            amplitude_contrast = input_star_parameters.ReturnAmplitudeContrast(list_of_averages_and_members[average_counter].class_members.Item(list_of_averages_and_members[average_counter].member_counter) - 1);
+
+            tmp_ctf.Init(microscope_voltage, spherical_abberation, amplitude_contrast, defocus_1, defocus_2, astigmatism_angle, pixel_size, additional_phase_shift);
+
+            // Ready image for summing
+            current_image.ForwardFFT( );
+            current_image.ZeroCentralPixel( );
+            apply_ctf(current_image, tmp_ctf, ctf_sum_of_squares_array);
+            current_image.BackwardFFT( );
+            current_image.Rotate2DInPlace(psi);
+            current_image.ForwardFFT( );
+            current_image.PhaseShift(-x_shift, -y_shift);
+
+            // Sum and increment
+            current_test_average.AddImage(&current_image);
+            list_of_averages_and_members[average_counter].member_counter++;
+        }
+
+        // Average is done; remove CTF, add to array, continue to next
+        divide_by_ctf_sum_of_squares(current_test_average, ctf_sum_of_squares_array);
+        tmp_averages[average_counter] = &current_test_average; // In complex space at this point
+    }
+
+    // Reset counters to 0 for subtraction
+    for ( int average_counter = 0; average_counter < number_of_averages; average_counter++ ) {
+        list_of_averages_and_members[average_counter].member_counter = 0;
+        tmp_averages[average_counter].QuickAndDirtyWriteSlice("raw_averages.mrc", average_counter + 1);
+    }
+
 #ifdef TMP_DEBUG
     wxPrintf("STAR file read; now about to enter rotate_shift_scale_subtract.\n");
 #endif
 
-    align_scale_subtract(particle_stack_mrc, classification_mrc, input_star_parameters, list_of_averages_and_members, number_of_averages, number_of_images);
+    align_scale_subtract(particle_stack_mrc, tmp_averages, input_star_parameters, list_of_averages_and_members, number_of_averages, number_of_images);
 
     overall_finish            = wxDateTime::Now( );
     wxTimeSpan time_to_finish = overall_finish.Subtract(overall_start);
@@ -154,13 +222,13 @@ bool MembraneSubtraction::DoCalculation( ) {
  * @brief Aligns the class average, scales it, and then subtracts it from the full stack.
  * 
  * @param particle_mrc Particle stack associated with the refinement package that contains the needed classification
- * @param classification_mrc Classification mrc that contains all class averages from the selected classification
+ * @param raw_averages_to_subtract Re-made averages that lack filtering applied by cisTEM 2D classification
  * @param ctf_params CTF parameters read in from .star file
  * @param class_members Array containing the list of all the members
  * @param list_of_averages_and_members Vector containing the grouped class averages and their included members
  * @param number_of_images Number of images in the full particle stack
  */
-void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images) {
+void align_scale_subtract(MRCFile& particle_mrc, Image* raw_averages_to_subtract, cisTEMParameters& ctf_params, ClassAveragesAndMembers* list_of_averages_and_members, const int number_of_averages, long number_of_images) {
     Image current_image;
     Image class_average_image;
     CTF   current_ctf;
@@ -196,7 +264,7 @@ void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, ci
         for ( int class_average_counter = 0; class_average_counter < number_of_averages; class_average_counter++ ) {
             if ( list_of_averages_and_members[class_average_counter].member_counter < list_of_averages_and_members[class_average_counter].number_of_members ) {
                 if ( list_of_averages_and_members[class_average_counter].class_members.Item(list_of_averages_and_members[class_average_counter].member_counter) - 1 == image_counter ) {
-                    class_average_image.ReadSlice(&classification_mrc, list_of_averages_and_members[class_average_counter].class_average_index);
+                    class_average_image.CopyFrom(&raw_averages_to_subtract[class_average_counter]);
 
                     // -1 because the cisTEMParameters class contains an array of all params -- this means 0 indexing
                     current_member = list_of_averages_and_members[class_average_counter].class_members.Item(list_of_averages_and_members[class_average_counter].member_counter) - 1;
@@ -212,14 +280,15 @@ void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, ci
 
                     //Apply to class average
                     current_ctf.Init(microscope_voltage, spherical_abberation, amplitude_contrast, defocus_1, defocus_2, astigmatism_angle, pixel_size, additional_phase_shift);
-                    class_average_image.ForwardFFT( );
                     class_average_image.ZeroCentralPixel( );
                     class_average_image.ApplyCTF(current_ctf);
                     class_average_image.BackwardFFT( );
 
                     // Align
                     class_average_image.Rotate2DInPlace(psi);
+                    class_average_image.ForwardFFT( );
                     class_average_image.PhaseShift(-x_shift, -y_shift);
+                    class_average_image.BackwardFFT( );
                     class_average_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", image_counter + 1);
 
                     // Scale: A * B / A * A
@@ -243,7 +312,80 @@ void align_scale_subtract(MRCFile& particle_mrc, MRCFile& classification_mrc, ci
         }
         if ( ! image_is_already_written ) {
             current_image.QuickAndDirtyWriteSlice("subtracted_stack.mrc", image_counter + 1);
-            current_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", image_counter + 1); // Filler for sanity check -- ask Tim how to improve this
+            current_image.SetToConstant(0.0);
+            current_image.QuickAndDirtyWriteSlice("aligned_ctf_average.mrc", image_counter + 1);
+        }
+    }
+}
+
+/**
+ * @brief Applies CTF to image while also calculating the sum of squares while looping through images.
+ * 
+ * @param current_image Image with CTF being applied
+ * @param ctf_to_apply CTF being applied to the image
+ * @param ctf_sum_of_squares Sum of squares of all CTF values in the stack used for later dividing
+ * @param absolute Default false; option to apply phase flip with CTF
+ */
+void apply_ctf(Image& current_image, CTF& ctf_to_apply, float* ctf_sum_of_squares, bool absolute) {
+    float y_coord_sq;
+    float x_coord_sq;
+
+    float y_coord;
+    float x_coord;
+
+    float frequency_squared;
+    float azimuth;
+    float ctf_value;
+
+    long pixel_counter = 0;
+
+    for ( int j = 0; j <= current_image.physical_upper_bound_complex_y; j++ ) {
+        y_coord    = current_image.ReturnFourierLogicalCoordGivenPhysicalCoord_Y(j) * current_image.fourier_voxel_size_y;
+        y_coord_sq = powf(y_coord, 2.0);
+
+        for ( int i = 0; i <= current_image.physical_upper_bound_complex_x; i++ ) {
+            x_coord    = i * current_image.fourier_voxel_size_x;
+            x_coord_sq = powf(x_coord, 2.0);
+
+            // Compute the azimuth
+            if ( i == 0 && j == 0 ) {
+                azimuth = 0.0;
+            }
+            else {
+                azimuth = atan2f(y_coord, x_coord);
+            }
+
+            // Compute the square of the frequency
+            frequency_squared = x_coord_sq + y_coord_sq;
+            ctf_value         = ctf_to_apply.Evaluate(frequency_squared, azimuth);
+
+            // phase-flip
+            if ( absolute )
+                ctf_value = fabsf(ctf_value);
+
+            current_image.complex_values[pixel_counter] *= ctf_value;
+            ctf_sum_of_squares[pixel_counter] += powf(ctf_value, 2);
+            pixel_counter++;
+        }
+    }
+}
+
+/**
+ * @brief Removes the CTF from the image sum via dividing by the square root of the ctf_sum_of_squares
+ * for each pixel
+ * 
+ * @param current_image Summed image that will ctf removed
+ * @param ctf_sum_of_squares Sum of squares of the images in the stack that will be removed from sum
+ */
+void divide_by_ctf_sum_of_squares(Image& current_image, float* ctf_sum_of_squares) {
+    // normalize by sum of squared CTFs (voxel by voxel)
+    long pixel_counter = 0;
+
+    for ( int j = 0; j <= current_image.physical_upper_bound_complex_y; j++ ) {
+        for ( int i = 0; i <= current_image.physical_upper_bound_complex_x; i++ ) {
+            if ( ctf_sum_of_squares[pixel_counter] != 0.0 )
+                current_image.complex_values[pixel_counter] /= sqrtf(ctf_sum_of_squares[pixel_counter]);
+            pixel_counter++;
         }
     }
 }
